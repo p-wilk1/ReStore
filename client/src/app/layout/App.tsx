9f9294b4c32a521f77e6ecfffdb49a9f35a573fc
Piotr Wilk <piotrix12@gmail.com>
2023-08-10 13:09:51 +0200

    Styling with MUI

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,38 +1,35 @@
-import { useEffect, useState } from "react";
-import { Product } from "../models/products";
+import Catalog from "../../features/catalog/Catalog";
+import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import Header from "./Header";
+import { useState } from "react";
 
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode]=useState(false);
+  const paletteType=darkMode ? 'dark': 'light';
+  const theme = createTheme({
+    palette:{
+      mode: paletteType,
+      background:{
+        default: paletteType==='light'? '#eaeaea' : '#121212'
+      }
+    }
+  })
 
-  useEffect(() => {
-    fetch('http://localhost:5101/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data))
-  }, [])
-
-function addProduct(){
-  setProducts(prevState=> [...prevState, 
-    {
-      id: prevState.length + 101,
-      name: 'product' + (prevState.length+1), 
-      price: 300.00 ,
-      brand: 'some brand',
-      description: 'some desc',
-      pictureUrl: 'https://picsum.photos/200'
-    }])
-}
+  function handleThemeChange(){
+    setDarkMode(!darkMode);
+  }
+  
 
   return (
-    <div>
-      <h1>Re-Store</h1>
-      <ul>
-        {products.map(product =>(
-          <li key={product.id}>{product.name} - {product.price}</li>
-        ))}
-      </ul>
-      <button onClick={addProduct}>Add Product</button>
-    </div>
+    <ThemeProvider theme={theme}>
+    <CssBaseline/>
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+        <Container><Catalog/>
+      </Container>
+      
+      
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,21 @@
+import { Product } from "../../app/models/products";
+import ProductList from "./ProductList";
+import { useState, useEffect } from "react";
+
+
+export default function Catalog(){
+    const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5101/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data))
+  }, [])
+
+
+    return(
+        <>
+        <ProductList products={products}/>
+        </>
+    )
+}
