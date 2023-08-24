12d8e98ce6627f8d84506eac5f469b367325a2fe
Piotr Wilk <piotrix12@gmail.com>
2023-08-24 18:31:30 +0200

    Routing and some styling

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -2,20 +2,18 @@ import { Product } from "../../app/models/products";
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
 
-
-export default function Catalog(){
-    const [products, setProducts] = useState<Product[]>([]);
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:5101/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data))
-  }, [])
+    fetch("http://localhost:5101/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-
-    return(
-        <>
-        <ProductList products={products}/>
-        </>
-    )
+  return (
+    <>
+      <ProductList products={products} />
+    </>
+  );
 }
